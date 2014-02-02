baa574e9ff2899b33520f2992d6965bd3fd44ee5
dewgeek <mailme.rakeshg@gmail.com>
2014-02-03 03:26:32 +0530

    Test

--- edit_node.php
+++ edit_node.php
@@ -10,32 +10,16 @@
 <!DOCTYPE html>
 <html lang="en">
 	<head>
-		<link href="css/bootstrap.min.css" rel="stylesheet">
-		<meta name="viewport" content="width=device-width, initial-scale=1.0">
-		<link href="css/bootstrap-responsive.min.css" rel="stylesheet">
+		
 	</head>
 
 	<body>
 		<div class="container-fluid">
 			<div class="row-fluid">
 
-				<div class="span6 offset3">
-					<ul class="nav nav-tabs">
-						<li style="padding-right: 10%" class="active"><a href="node.php">Node</a></li>
-						<li style="padding-right: 10%"><a href="graph.php">Graph</a></li>
-						<li><a href="decision.php">Decision</a></li>
-					</ul>
-				</div>
-
-				<div class="span6 offset3">
-					<h2 class="text-center">Node</h2>
-					<ul class="nav nav-tabs">
-						<li style="padding-right: 10%"><a href="add_node.php">Add a Node</a></li>
-						<li class="active" style="padding-right: 10%"><a href="edit_node.php">Edit a Node</a></li>
-						<li><a href="delete_node.php">Delete a Node</a></li>
-					</ul>
-				</div>
-
+			<?php 
+				include 'navbar.html'
+			?>
 				<div class="span6 offset3">
 					<ul class="nav nav-pills nav-stacked">
 						<?php
--- index.php
+++ index.php
@@ -9,23 +9,18 @@
 <!DOCTYPE html>
 <html lang="en">
 	<head>
-		<link href="css/bootstrap.min.css" rel="stylesheet">
-		<meta name="viewport" content="width=device-width, initial-scale=1.0">
-		<link href="css/bootstrap-responsive.min.css" rel="stylesheet">
+		<link href="static/css/bootstrap.css" rel="stylesheet">
 	</head>
 
 	<body>
-		<div class="container-fluid">
-			<div class="row-fluid">
-				<div class="span6 offset3">
-					<h2 class="text-center">Home Page</h2>
-					<ul class="nav nav-tabs">
-						<li style="padding-right: 10%"><a href="node.php">Node</a></li>
-						<li style="padding-right: 10%"><a href="graph.php">Graph</a></li>
-						<li><a href="decision.php">Decision</a></li>
-					</ul>
-				</div>
-			</div>
-		</div>
+	
+	<div class="container">
+		
+		<?php 
+			include 'navbar.html' 
+		?>	
+		
+	</div>
+		
 	</body>
 </html>
