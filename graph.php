1a6dc6fc0d6b5a18db269e753e3558f3312496d4
dewgeek <mailme.rakeshg@gmail.com>
2014-02-03 17:23:53 +0530

    Added form in Graph.php and added an ajx query

--- graph.php
+++ graph.php
@@ -9,43 +9,89 @@
 
 <!DOCTYPE html>
 <meta charset="utf-8">
-<style>
-
-body {
-  font: 10px sans-serif;
-}
-
-.axis path,
-.axis line {
-  fill: none;
-  stroke: #000;
-  shape-rendering: crispEdges;
-}
-
-.x.axis path {
-  display: none;
-}
-
-.line {
-  fill: none;
-  stroke: 'steelblue';
-  stroke-width: 1.5px;
-}
-
-</style>
-
 <html lang="en">
   <head>
     <title>Graph</title>
     <link href="static/css/bootstrap.css" rel="stylesheet">
     
-  </head>
+  
+  <style type="text/css">
 
+  .custom-btn{
+    width:100%;
+    background-color: #59407F;
+    color:#fff;
+  }
+
+    .axis path,
+    .axis line {
+      fill: none;
+      stroke: #000;
+      shape-rendering: crispEdges;
+    }
+
+    .x.axis path {
+      display: none;
+    }
+
+    .line {
+      fill: none;
+      stroke: 'steelblue';
+      stroke-width: 1.5px;
+    }
+
+
+    .date-icon{
+      margin-left: -25px;
+    }
+  </style>
+  </head>
   <body>
     <div class="container">
       <?php 
         include 'navbar.html'
       ?>
+  
+      <form class="form" role="form" id="plotDetailForm">
+        <div class="row">
+          <div class="col-sm-2"><h5>Date range</h5></div>
+          <div class="col-sm-4">
+            <div class="row">
+              <div class="col-xs-10"><input type="text" class="form-control" id="startDate" name="startDate"  placeholder="start date"></div>
+              <div class="col-xs-2 date-icon"><a href="#" id="startDatePicker"><span class=" glyphicon glyphicon-calendar" id="startDatePicker"></span></a></div>
+            </div>
+            
+            
+          </div>
+          <div class="col-sm-4">
+            <div class="row">
+              <div class="col-xs-10"><input type="text" class="form-control" id="endDate" name="endDate" placeholder="end date"></div>
+              <div class="col-xs-2 date-icon"><a href="#" id="endDatePicker"><span id="endDatePicker" class=" glyphicon glyphicon-calendar"></span></a></div>
+            </div>
+          </div>
+          <div class="col-sm-4"></div>
+        </div>
+        <div class="row">
+          <div class="col-md-2">Node</div>
+          <div class="col-md-2"><input type="radio" name="node" value="node1" id="node1"><label for="node1">Node1</label></div>
+          <div class="col-md-2"><input type="radio" name="node" value="node2" id="node2"><label for="node2">Node2</label></div>
+          <div class="col-md-2"><input type="radio" name="node" value="node3" id="node3"><label for="node3">node3</label></div>
+          <div class="col-md-2"><input type="radio" name="node" value="node4" id="node4"><label for="node4">node4</label></div>
+          <div class="col-md-2"></div>
+        </div>
+        <div class="row">
+          <div class="col-md-2">Node</div>
+          <div class="col-md-2"><input type="checkbox" name="port" value="portA" id="portA"><label for="portA">portA</label></div>
+          <div class="col-md-2"><input type="checkbox" name="port" value="portB" id="portB"><label for="portB">portB</label></div>
+          <div class="col-md-2"><input type="checkbox" name="port" value="portC" id="portC"><label for="portC">portC</label></div>
+          <div class="col-md-2"><input type="checkbox" name="port" value="portD" id="portD"><label for="portD">portD</label></div>
+        </div>
+        <div class="row">
+          <div class="col-md-2"><button class="custom-btn btn btn-sm" id="plotGraph"><b>Plot Graph</b></button></div>
+          <div class="col-md-10"></div>
+        </div>
+      </form>
+
     </div>
   </body>
 </html>
